b93d4829b28989ade7acb4db6d0d2ad290621e3d
SivanJhirad <sivanush550@gamil.com>
2022-04-01 10:28:11 +0300

    logic works

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Register from './Register';
-import Signin from './Signin';
+import Register from './register/Register';
+import Signin from './sign/Signin';
+import Chats from './chats/Chats';
 
 import {BrowserRouter, Routes, Route, Router, Link} from 'react-router-dom';
 
+
+
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Signin />}></Route>    
-        <Route path="/register" element={<Register />}></Route>
-      </Routes>
-      </BrowserRouter>
-
-    </div>
-  );
+      <div className="App">
+        <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Signin/>}></Route>    
+          <Route path="/register" element={<Register/>}></Route>
+          <Route path='/chats' element={<Chats/>}></Route>
+        </Routes>
+        </BrowserRouter>  
+      </div>
+    );
 }
 
 export default App;
+
+
