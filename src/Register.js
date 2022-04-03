34ee302a56face25ea5a4ac72dbc67630abc7ffb
Ofri Zangi <ofrizangi@gmail.com>
2022-04-03 13:27:28 +0300

    commit

--- src/Register.js
+++ src/Register.js
@@ -19,16 +19,16 @@ function Register() {
     
     const chekUserName = function() {
         let UserNameExsist = false
-        //var user = document.getElementById('username').value;
+        var user = document.getElementById('username').value;
 
         var messageUser = document.getElementById('userNameValid');
 
         for (let i = 0; i < users.length; i++) {
-            if (users[i].userName === userName) {
+            if (users[i].userName === user) {
                 UserNameExsist = true;
             }}
 
-        if (UserNameExsist || userName === ''){
+        if (UserNameExsist || user === ''){
             messageUser.innerHTML = '<p>user is not valid</p>';
         }
         else {
@@ -41,15 +41,15 @@ function Register() {
         var pass = document.getElementById('Password').value
         var message = document.getElementById('StrongPass')
         if (pass.length < 8 || !(/\d/.test(pass)) || !(/[a-zA-Z]/.test(pass))) {
-            message.innerHTML = '<p id="notValid" >not valid</p>'
+            message.innerHTML = '<div id="notValid" >not valid</div>'
             return false;
         }
         else if (pass.match(/\d/g)?.length <= 2 || pass.match(/[a-zA-Z]/g)?.length <= 2) {
-            message.innerHTML = '<p id="weak">weak</p>'
+            message.innerHTML = '<div id="weak">weak</div>'
             return true;
         }
         else {
-            message.innerHTML = '<p id="strong">strong</p>'
+            message.innerHTML = '<div id="strong">strong</div>'
             return true;
         }
     }
@@ -87,9 +87,14 @@ function Register() {
 */
 return (
     <div className='center'>
-    
+
+        <h1> Registration:</h1>
+        <br></br>
+
+        <form action="/action_page.php" method="get">
+
         <div className="form-floating">
-            <input type="text" className="form-control" id="username" name="username" placeholder="User Name" onInput={chekUserName} onChange={event => setUserName(event.target.value)}></input>
+            <input type="text" className="form-control" id="username" name="username" placeholder="User Name" onInput={chekUserName} onChange={event => setUserName(event.target.value) } required ></input>
             <label htmlFor="username">User Name</label>
         </div>
         <span id="userNameValid"></span> <br></br>
@@ -101,36 +106,33 @@ return (
 
     
         <div className="mb-3">
-            <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-
+            {/* <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label> */}
+            <label></label>
             <input type="file" className="form-control" id="fname" name="fname" accept="image/png, image/jpeg" onChange={event => setImage(event.target.value)}></input> <br></br>
         </div>
 
         <div className="form-floating">
-            <input type="password" className="form-control" id="Password" name="pass" placeholder="Password" onInput={checkPass2} onChange={event => setPassword(event.target.value)}>
-
-                
-                 </input>
-                 <i className="bi-info-circle" data-bs-toggle="tooltip" data-bs-placement="right"
-            title="password must be at least 8 charecters, contains at least one number and letter"></i>
+            <input type="password" className="form-control" id="Password" name="pass" placeholder="Password" onInput={checkPass2} onChange={event => setPassword(event.target.value)}>    
+            </input>
+            <i className="bi-info-circle" data-bs-toggle="tooltip" data-bs-placement="right"
+            title="password must be at least 8 charecters, you must have at least one number and letter"></i>
             <label htmlFor="Password">Password</label>
-    
-     
-</div>
+            <span id="StrongPass"></span> <br></br>
 
-        <span id="StrongPass"></span> <br></br>
+        </div>
+
 
         <div className="form-floating">
             <input type="password" className="form-control" id="checkPass" nume="pass" placeholder="Check Password"></input>
             <label htmlFor="checkPass">Check Password</label>
         </div>
 
-
-        <button type="button" className="btn btn-primary" id="submitButten" onClick={shoot}>Submit</button>
+        <br></br>
+        <button type="submit" className="btn btn-primary" id="submitButten" onClick={shoot}>Submit</button>
 
         <Link to='/'>Sign in</Link>
 
-
+        </form>
 
   </div>    );
 }
