043ec266f1e6c9e9505755a4adfa46f154b9135c
SivanJhirad <sivanush550@gamil.com>
2022-04-17 01:13:59 +0300

    new user

--- src/chat/Chat.js
+++ src/chat/Chat.js
@@ -16,9 +16,16 @@ function Chat() {
   const [userChatPrassed, setUserChatPrassed] = useState(null);
 
   const getArrOfUserContact = function(){
-    
+    //מוצא את האינדקס של האיש קשר במערך של המשתמשים
+    //הבעיה שלא הוספנו את המשתמש החדש כאיש קשר ליתר המשתמשים
       let index1 = users.findIndex(x => (x.userName ===  userChatPrassed.userName))
       let index2 = users[index1].contacts.findIndex(x => (x.userName ===  user.userName))
+      //המשתמש החדש לא קיים כאיש קשר אצל האיש קשר שאליו שלח הודעה
+      if(index2===-1){
+        users[index1].contacts= [...users[index1].contacts,{userName: user.userName,image:user.image, messages:[] }]
+        index2=users[index1].contacts.length-1
+      }
+    
 
       return users[index1].contacts[index2];
   }
