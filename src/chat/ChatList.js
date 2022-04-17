2358fb58d8beb82181781f37e51f3d959eb10184
Ofri Zangi <ofrizangi@gmail.com>
2022-04-17 16:55:08 +0300

    finished audio + time

--- src/chat/ChatList.js
+++ src/chat/ChatList.js
@@ -8,7 +8,6 @@ function ChatList(props){
 
     return(
 
-
         <div className="list-group list-group-flush">
             { props.contacts.map((item)=> <ChatItem contact={item} setUser={props.setUser} setMessages = {props.setMessages}/>) }
         </div> 
