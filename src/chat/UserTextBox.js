86370dd23bf1b5d7a3d8f908733a380c28bc1a5b
Ofri Zangi <88827751+ofrizangi@users.noreply.github.com>
2022-04-18 12:07:55 +0300

    Update UserTextBox.js

--- src/chat/UserTextBox.js
+++ src/chat/UserTextBox.js
@@ -4,6 +4,12 @@ import './message.css'
 import MessageBox from "./MessageBox.js"
 
 
+import SendMessage from "./SendMessage";
+import './chat.css'
+import './message.css'
+import MessageBox from "./MessageBox.js"
+
+
 function UserTextBox(props) {
 
     // delete me
@@ -14,12 +20,12 @@ function UserTextBox(props) {
 
             <div className="top-row list-group-item d-flex align-items-center">
                 <img src={props.item.image} alt="Avatar" className="avatar"></img>
-                <span className="w-100 m-2 ms-3 nameContact" >{props.item.userName}</span>
+                <span className="w-100 m-2 ms-3 nameContact chat-name" >{props.item.nickName}</span>
 
             </div>
             <div className="message-scroll">
                 {/* {props.messages.map((item) => item.sentByMe ? <SendMessageBox item={item} /> : <RecieveMessageBox item={item} />)} */}
-                {props.messages.map((item) => <MessageBox key={item.message} item={item} />)}
+                {props.messages.map((item) => <MessageBox item={item} />)}
 
             </div>
 
